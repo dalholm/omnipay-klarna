3ec9b05a198f41e39a49fdbd0ed3a48c0109c40c
David <david@nyehandel.se>
2026-02-24 14:54:12 +0100

    Only use the new kustom base url

--- src/Gateway.php
+++ src/Gateway.php
@@ -24,11 +24,9 @@ final class Gateway extends AbstractGateway implements GatewayInterface
 
     const API_VERSION_NORTH_AMERICA = 'NA';
 
-    const EU_BASE_URL = 'https://api.klarna.com';
-    const EU_KUSTOM_BASE_URL = 'https://api.kustom.co';
+    const EU_BASE_URL = 'https://api.kustom.co';
 
-    const EU_TEST_KUSTOM_BASE_URL = 'https://api.playground.kustom.co';
-    const EU_TEST_BASE_URL = 'https://api.playground.klarna.com';
+    const EU_TEST_BASE_URL = 'https://api.playground.kustom.co';
     const NA_BASE_URL = 'https://api-na.klarna.com';
     const NA_TEST_BASE_URL = 'https://api-na.playground.klarna.com';
 
@@ -267,20 +265,12 @@ final class Gateway extends AbstractGateway implements GatewayInterface
 
     public function getTestUrl()
     {
-        if(str_starts_with( $this->getUsername(), 'PM')) {
-            return self::EU_TEST_KUSTOM_BASE_URL;
-        } else {
-            return self::EU_TEST_BASE_URL;
-        }
+        return self::EU_TEST_BASE_URL;
     }
 
     public function getBaseUrl()
     {
-        if(str_starts_with( $this->getUsername(), 'M')) {
-            return self::EU_KUSTOM_BASE_URL;
-        } else {
-            return self::EU_BASE_URL;
-        }
+        return self::EU_BASE_URL;
     }
 
     private function setBaseUrl()
